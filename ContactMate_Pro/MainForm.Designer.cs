95cbe3fd4b223c5a1c2dc0637600f3905f792110
enesnic <enesnic2@gmail.com>
2023-10-06 17:05:48 +0800

    BASE DESIGN FOR MAIN FORM

--- ContactMate_Pro/MainForm.Designer.cs
+++ ContactMate_Pro/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ContactMate_Pro
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,28 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.elipse = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.SuspendLayout();
+            // 
+            // elipse
+            // 
+            this.elipse.TargetControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Elipse elipse;
     }
 }
 
--- ContactMate_Pro/Program.cs
+++ ContactMate_Pro/Program.cs
@@ -16,7 +16,7 @@ namespace ContactMate_Pro
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
