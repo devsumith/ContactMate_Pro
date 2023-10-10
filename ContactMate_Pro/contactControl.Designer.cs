23ac10d9db82d2a4fba30055ea2281f20cc1d793
enesnic <enesnic2@gmail.com>
2023-10-10 20:13:00 +0800

    UPDATE UPDATE!

--- ContactMate_Pro/contactControl.Designer.cs
+++ ContactMate_Pro/contactControl.Designer.cs
@@ -31,13 +31,11 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(contactControl));
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.guna2CustomGradientPanel18 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
             this.guna2HtmlLabel9 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2HtmlLabel8 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2HtmlLabel7 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
-            this.guna2CustomGradientPanel18.SuspendLayout();
             this.guna2Panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -47,33 +45,15 @@
             this.guna2Elipse1.BorderRadius = 10;
             this.guna2Elipse1.TargetControl = this;
             // 
-            // guna2CustomGradientPanel18
-            // 
-            this.guna2CustomGradientPanel18.BorderRadius = 10;
-            this.guna2CustomGradientPanel18.Controls.Add(this.guna2HtmlLabel9);
-            this.guna2CustomGradientPanel18.Controls.Add(this.guna2HtmlLabel8);
-            this.guna2CustomGradientPanel18.Controls.Add(this.guna2HtmlLabel7);
-            this.guna2CustomGradientPanel18.Controls.Add(this.guna2Panel1);
-            this.guna2CustomGradientPanel18.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.guna2CustomGradientPanel18.FillColor = System.Drawing.Color.LimeGreen;
-            this.guna2CustomGradientPanel18.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(187)))), ((int)(((byte)(106)))));
-            this.guna2CustomGradientPanel18.FillColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.guna2CustomGradientPanel18.FillColor4 = System.Drawing.Color.SteelBlue;
-            this.guna2CustomGradientPanel18.Location = new System.Drawing.Point(0, 0);
-            this.guna2CustomGradientPanel18.Name = "guna2CustomGradientPanel18";
-            this.guna2CustomGradientPanel18.Size = new System.Drawing.Size(226, 114);
-            this.guna2CustomGradientPanel18.TabIndex = 1;
-            //this.guna2CustomGradientPanel18.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2CustomGradientPanel18_Paint);
-            // 
             // guna2HtmlLabel9
             // 
             this.guna2HtmlLabel9.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel9.Font = new System.Drawing.Font("Bahnschrift Condensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel9.ForeColor = System.Drawing.Color.MintCream;
-            this.guna2HtmlLabel9.Location = new System.Drawing.Point(73, 51);
+            this.guna2HtmlLabel9.Location = new System.Drawing.Point(77, 51);
             this.guna2HtmlLabel9.Name = "guna2HtmlLabel9";
             this.guna2HtmlLabel9.Size = new System.Drawing.Size(138, 20);
-            this.guna2HtmlLabel9.TabIndex = 30;
+            this.guna2HtmlLabel9.TabIndex = 34;
             this.guna2HtmlLabel9.Text = "jirolaurenz0101@gmail.com";
             // 
             // guna2HtmlLabel8
@@ -81,10 +61,10 @@
             this.guna2HtmlLabel8.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel8.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel8.ForeColor = System.Drawing.Color.MintCream;
-            this.guna2HtmlLabel8.Location = new System.Drawing.Point(72, 28);
+            this.guna2HtmlLabel8.Location = new System.Drawing.Point(76, 28);
             this.guna2HtmlLabel8.Name = "guna2HtmlLabel8";
             this.guna2HtmlLabel8.Size = new System.Drawing.Size(87, 25);
-            this.guna2HtmlLabel8.TabIndex = 29;
+            this.guna2HtmlLabel8.TabIndex = 33;
             this.guna2HtmlLabel8.Text = "09392723942";
             // 
             // guna2HtmlLabel7
@@ -92,10 +72,10 @@
             this.guna2HtmlLabel7.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel7.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel7.ForeColor = System.Drawing.Color.Honeydew;
-            this.guna2HtmlLabel7.Location = new System.Drawing.Point(65, 76);
+            this.guna2HtmlLabel7.Location = new System.Drawing.Point(69, 76);
             this.guna2HtmlLabel7.Name = "guna2HtmlLabel7";
             this.guna2HtmlLabel7.Size = new System.Drawing.Size(96, 27);
-            this.guna2HtmlLabel7.TabIndex = 19;
+            this.guna2HtmlLabel7.TabIndex = 32;
             this.guna2HtmlLabel7.Text = "JIRO LAURENZ";
             // 
             // guna2Panel1
@@ -103,10 +83,10 @@
             this.guna2Panel1.BorderRadius = 30;
             this.guna2Panel1.Controls.Add(this.guna2CirclePictureBox1);
             this.guna2Panel1.FillColor = System.Drawing.Color.Honeydew;
-            this.guna2Panel1.Location = new System.Drawing.Point(8, 11);
+            this.guna2Panel1.Location = new System.Drawing.Point(12, 11);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.Size = new System.Drawing.Size(60, 60);
-            this.guna2Panel1.TabIndex = 0;
+            this.guna2Panel1.TabIndex = 31;
             // 
             // guna2CirclePictureBox1
             // 
@@ -129,21 +109,22 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
-            this.Controls.Add(this.guna2CustomGradientPanel18);
+            this.Controls.Add(this.guna2HtmlLabel9);
+            this.Controls.Add(this.guna2HtmlLabel8);
+            this.Controls.Add(this.guna2HtmlLabel7);
+            this.Controls.Add(this.guna2Panel1);
             this.Name = "contactControl";
             this.Size = new System.Drawing.Size(226, 114);
-            this.guna2CustomGradientPanel18.ResumeLayout(false);
-            this.guna2CustomGradientPanel18.PerformLayout();
             this.guna2Panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
-        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel18;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel9;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel8;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel7;
