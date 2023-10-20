92460bad9cdbe3fd0ca13bbdd2ee327c1c1ee6f3
enesnic <enesnic2@gmail.com>
2023-10-20 19:39:24 +0800

    UPDATE FOR THE HEAD PANEL

--- ContactMate_Pro/Forms/ContactDetailsForm.Designer.cs
+++ ContactMate_Pro/Forms/ContactDetailsForm.Designer.cs
@@ -34,8 +34,10 @@
             this.guna2CustomGradientPanel2 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
             this.selectImageBtn = new Guna.UI2.WinForms.Guna2CircleButton();
             this.contactImageBox = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2CustomGradientPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.contactImageBox)).BeginInit();
+            this.guna2Panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // borderlessForm
@@ -57,7 +59,7 @@
             this.closeBtn.FillColor = System.Drawing.Color.Transparent;
             this.closeBtn.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.closeBtn.IconColor = System.Drawing.Color.Black;
-            this.closeBtn.Location = new System.Drawing.Point(433, 6);
+            this.closeBtn.Location = new System.Drawing.Point(337, 6);
             this.closeBtn.Name = "closeBtn";
             this.closeBtn.Size = new System.Drawing.Size(38, 25);
             this.closeBtn.TabIndex = 15;
@@ -65,17 +67,17 @@
             // 
             // guna2CustomGradientPanel2
             // 
-            this.guna2CustomGradientPanel2.BackColor = System.Drawing.Color.Honeydew;
-            this.guna2CustomGradientPanel2.BorderRadius = 10;
+            this.guna2CustomGradientPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2CustomGradientPanel2.BorderRadius = 70;
             this.guna2CustomGradientPanel2.Controls.Add(this.selectImageBtn);
             this.guna2CustomGradientPanel2.Controls.Add(this.contactImageBox);
             this.guna2CustomGradientPanel2.FillColor = System.Drawing.Color.LimeGreen;
             this.guna2CustomGradientPanel2.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(187)))), ((int)(((byte)(106)))));
             this.guna2CustomGradientPanel2.FillColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.guna2CustomGradientPanel2.FillColor4 = System.Drawing.Color.SteelBlue;
-            this.guna2CustomGradientPanel2.Location = new System.Drawing.Point(12, 43);
+            this.guna2CustomGradientPanel2.Location = new System.Drawing.Point(12, 47);
             this.guna2CustomGradientPanel2.Name = "guna2CustomGradientPanel2";
-            this.guna2CustomGradientPanel2.Size = new System.Drawing.Size(455, 162);
+            this.guna2CustomGradientPanel2.Size = new System.Drawing.Size(150, 150);
             this.guna2CustomGradientPanel2.TabIndex = 30;
             // 
             // selectImageBtn
@@ -91,7 +93,7 @@
             this.selectImageBtn.ForeColor = System.Drawing.Color.White;
             this.selectImageBtn.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(108)))), ((int)(((byte)(122)))), ((int)(((byte)(137)))));
             this.selectImageBtn.ImageSize = new System.Drawing.Size(35, 35);
-            this.selectImageBtn.Location = new System.Drawing.Point(10, 11);
+            this.selectImageBtn.Location = new System.Drawing.Point(5, 5);
             this.selectImageBtn.Name = "selectImageBtn";
             this.selectImageBtn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.selectImageBtn.Size = new System.Drawing.Size(140, 140);
@@ -107,7 +109,7 @@
             this.contactImageBox.FillColor = System.Drawing.Color.Honeydew;
             this.contactImageBox.Image = global::ContactMate_Pro.Properties.Resources.user__2_;
             this.contactImageBox.ImageRotate = 0F;
-            this.contactImageBox.Location = new System.Drawing.Point(10, 11);
+            this.contactImageBox.Location = new System.Drawing.Point(5, 5);
             this.contactImageBox.Name = "contactImageBox";
             this.contactImageBox.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.contactImageBox.Size = new System.Drawing.Size(140, 140);
@@ -115,20 +117,31 @@
             this.contactImageBox.TabIndex = 0;
             this.contactImageBox.TabStop = false;
             // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.LightGreen;
+            this.guna2Panel1.Controls.Add(this.closeBtn);
+            this.guna2Panel1.Controls.Add(this.guna2CustomGradientPanel2);
+            this.guna2Panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(383, 210);
+            this.guna2Panel1.TabIndex = 31;
+            // 
             // ContactDetailsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.MintCream;
-            this.ClientSize = new System.Drawing.Size(479, 548);
-            this.Controls.Add(this.guna2CustomGradientPanel2);
-            this.Controls.Add(this.closeBtn);
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(383, 548);
+            this.Controls.Add(this.guna2Panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ContactDetailsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "hind";
             this.guna2CustomGradientPanel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.contactImageBox)).EndInit();
+            this.guna2Panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -140,5 +153,6 @@
         private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel2;
         private Guna.UI2.WinForms.Guna2CirclePictureBox contactImageBox;
         public Guna.UI2.WinForms.Guna2CircleButton selectImageBtn;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
     }
 }
