bb16bbd7a41a42fa9c2804d149acc9122318c147
enesnic <enesnic2@gmail.com>
2023-10-24 15:08:51 +0800

    LOGO FOR BUTTON TO ADD GMAIL ADDRESS

--- ContactMate_Pro/Properties/Resources.Designer.cs
+++ ContactMate_Pro/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace ContactMate_Pro.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap gmail {
+            get {
+                object obj = ResourceManager.GetObject("gmail", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap left_chevron {
             get {
                 object obj = ResourceManager.GetObject("left_chevron", resourceCulture);
