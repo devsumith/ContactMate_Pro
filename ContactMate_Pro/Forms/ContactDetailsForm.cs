23a5a682d62612bb34e5c9ce6e0e18e3666c5b75
enesnic <enesnic2@gmail.com>
2023-11-04 21:07:04 +0800

    ADD A KEYS FUNCTION FOR cGmailBox

--- ContactMate_Pro/Forms/ContactDetailsForm.cs
+++ ContactMate_Pro/Forms/ContactDetailsForm.cs
@@ -153,7 +153,10 @@ namespace ContactMate_Pro
 
         private void cGmailBox_KeyDown(object sender, KeyEventArgs e)
         {
-
+            // When the Enter key is pressed in the cGmailBox control, 
+            // simulate a click event on the addCGmailBt button.
+            if (e.KeyCode == Keys.Enter)
+                addCGmailBtn_Click(sender, e);
         }
 
         #endregion
