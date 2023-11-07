91b6caf2786f663c7835c1b28b8cea21a05e8b1a
enesnic <enesnic2@gmail.com>
2023-11-07 20:23:13 +0800

    NEW CLASS FOR CONTACT DETAILS

---
+++ ContactMate_Pro/Classes/ContactDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactMate_Pro
+{
+    public class ContactDetails
+    {
+        public string Nickname { get; set; }
+        public string Relationship { get; set; }
+        public List<string> ContactNumber { get; set; }
+
+    }
+}
